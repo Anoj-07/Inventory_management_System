5712d90d021fb6328bf7996c42de4f84ca720f2b
Anoj <anojrawal12345@gmail.com>
2025-07-25 08:39:22 +0545

    product view change

--- base/views.py
+++ base/views.py
@@ -121,21 +121,3 @@ class VendorApiView(GenericViewSet):
 class ProductApiView(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    # def list(self, request):
-    #     pass
-    
-    # def create(self, request):
-    #     pass
-    
-    # def update(self, request, pk):
-    #     pass
-    
-    # def retrieve(self, request, pk):
-    #     pass
-    
-    # def destroy(self, request, pk):
-    #     pass
-    
-    # def partial_update(self, request, pk):
-    #     pass
